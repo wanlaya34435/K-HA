3c48a1e85b37f5f21b92feabff46005e9582590a
Prajak.B <prajak.b@zealtechinter.com>
2022-11-10 14:51:52 +0700

    #prod test

--- src/main/java/com/zti/kha/WebappApplication.java
+++ src/main/java/com/zti/kha/WebappApplication.java
@@ -1,5 +1,6 @@
 package com.zti.kha;
 
+import com.mongodb.MongoClientOptions;
 import com.twelvemonkeys.servlet.image.IIOProviderContextListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,7 +54,7 @@ public class WebappApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-//        SSLContextHelper.setSslProperties();
+        SSLContextHelper.setSslProperties();
         SpringApplication.run(WebappApplication.class, args);
     }
 
@@ -72,46 +73,117 @@ public class WebappApplication extends SpringBootServletInitializer {
         localeResolver.setDefaultLocale(Locale.US);
         return localeResolver;
     }
+
+//    protected static class SSLContextHelper {
+//
+//        private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+//        private static final String DEFAULT_SSL_CERTIFICATE = "ssl/rds-combined-ca-bundle.pem";
+//        private static final String SSL_CERTIFICATE = "sslCertificate";
+//        private static final String KEY_STORE_TYPE = "JKS";
+//        private static final String KEY_STORE_PROVIDER = "SUN";
+//        private static final String KEY_STORE_FILE_PREFIX = "sys-connect-via-ssl-test-cacerts";
+//        private static final String KEY_STORE_FILE_SUFFIX = ".jks";
+//        private static final String DEFAULT_KEY_STORE_PASSWORD = "changeit";
+//        private static final String SSL_TRUST_STORE = "javax.net.ssl.trustStore";
+//        private static final String SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
+//        private static final String SSL_TRUST_STORE_TYPE = "javax.net.ssl.trustStoreType";
+//
+//
+//
+//        private static void setSslProperties() {
+//
+//            try {
+////                String sslCertificate= System.getProperty(SSL_CERTIFICATE);
+//                String sslCertificate = "C:/Users/ZTI61-13/Desktop/AWSDocDB/ap-southeast-1-bundle.p7b";
+////                if(ObjectUtils.isEmpty(sslCertificate)) {
+////                    sslCertificate= DEFAULT_SSL_CERTIFICATE;
+////                }
+//                logger.info(" ssl certificate path {}",sslCertificate);
+//                System.setProperty(SSL_TRUST_STORE, createKeyStoreFile(sslCertificate));
+//                System.setProperty(SSL_TRUST_STORE_TYPE, KEY_STORE_TYPE);
+//                System.setProperty(SSL_TRUST_STORE_PASSWORD, DEFAULT_KEY_STORE_PASSWORD);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//
+//        private static String createKeyStoreFile(String sslCertificate) throws Exception {
+//            return createKeyStoreFile(createCertificate(sslCertificate)).getPath();
+//        }
+//
+//        private static X509Certificate createCertificate(String sslCertificate) throws Exception {
+//            CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+//            URL url = new File(sslCertificate).toURI().toURL();
+//            if (url == null) {
+//                throw new Exception();
+//            }
+//            try (InputStream certInputStream = url.openStream()) {
+//                return (X509Certificate) certFactory.generateCertificate(certInputStream);
+//            }
+//        }
+//
+//        private static File createKeyStoreFile(X509Certificate rootX509Certificate) throws Exception {
+//            File keyStoreFile = File.createTempFile(KEY_STORE_FILE_PREFIX, KEY_STORE_FILE_SUFFIX);
+//            try (FileOutputStream fos = new FileOutputStream(keyStoreFile.getPath())) {
+//                KeyStore ks = KeyStore.getInstance(KEY_STORE_TYPE, KEY_STORE_PROVIDER);
+//                ks.load(null);
+//                ks.setCertificateEntry("rootCaCertificate", rootX509Certificate);
+//                ks.store(fos, DEFAULT_KEY_STORE_PASSWORD.toCharArray());
+//            }
+//            return keyStoreFile;
+//        }
+//
+//    }
+
     protected static class SSLContextHelper {
 
-        private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-        private static final String DEFAULT_SSL_CERTIFICATE = "rds-combined-ca-bundle.pem";
-        private static final String SSL_CERTIFICATE = "sslCertificate";
+        private static final Logger logger = LoggerFactory.getLogger(SSLContextHelper.class);
+//        private static final String SSL_CERTIFICATE = "C:/Users/ZTI61-13/Desktop/AWSDocDB/ap-southeast-1-bundle.p7b";
+//        private static final String SSL_CERTIFICATE = "ssl/rds-combined-ca-bundle.pem";
+        private static final String SSL_CERTIFICATE = "ssl/amz-ap-se1.cer";
+
         private static final String KEY_STORE_TYPE = "JKS";
         private static final String KEY_STORE_PROVIDER = "SUN";
         private static final String KEY_STORE_FILE_PREFIX = "sys-connect-via-ssl-test-cacerts";
         private static final String KEY_STORE_FILE_SUFFIX = ".jks";
-        private static final String DEFAULT_KEY_STORE_PASSWORD = "changeit";
-        private static final String SSL_TRUST_STORE = "javax.net.ssl.trustStore";
-        private static final String SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
-        private static final String SSL_TRUST_STORE_TYPE = "javax.net.ssl.trustStoreType";
+        private static final String DEFAULT_KEY_STORE_PASSWORD = "zeal1tech";
 
-
-
+        /**
+         * This method sets the SSL properties which specify the key store file, its type and password:
+         *
+         * @throws Exception
+         */
         private static void setSslProperties() {
 
             try {
-                String sslCertificate= System.getProperty(SSL_CERTIFICATE);
-                if(ObjectUtils.isEmpty(sslCertificate)) {
-                    sslCertificate= DEFAULT_SSL_CERTIFICATE;
-                }
-                logger.info(" ssl certificate path {}",sslCertificate);
-                System.setProperty(SSL_TRUST_STORE, createKeyStoreFile(sslCertificate));
-                System.setProperty(SSL_TRUST_STORE_TYPE, KEY_STORE_TYPE);
-                System.setProperty(SSL_TRUST_STORE_PASSWORD, DEFAULT_KEY_STORE_PASSWORD);
+//                System.setProperty("javax.net.ssl.trustStore", createKeyStoreFile());
+//                String keyPath = createKeyStoreFile();
+                String keyPath = "ssl/ap-se-store.jks";
+                logger.info(">>>>> keyPath: "+keyPath);
+                System.setProperty("javax.net.ssl.trustStore", keyPath);
             } catch (Exception e) {
+
                 e.printStackTrace();
             }
-
+            System.setProperty("javax.net.ssl.trustStoreType", KEY_STORE_TYPE);
+            System.setProperty("javax.net.ssl.trustStorePassword", DEFAULT_KEY_STORE_PASSWORD);
         }
 
-        private static String createKeyStoreFile(String sslCertificate) throws Exception {
-            return createKeyStoreFile(createCertificate(sslCertificate)).getPath();
+
+        private static String createKeyStoreFile() throws Exception {
+            return createKeyStoreFile(createCertificate()).getPath();
         }
 
-        private static X509Certificate createCertificate(String sslCertificate) throws Exception {
+        /**
+         * This method generates the SSL certificate
+         *
+         * @return
+         * @throws Exception
+         */
+        private static X509Certificate createCertificate() throws Exception {
             CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-            URL url = new File(sslCertificate).toURI().toURL();
+            URL url = new File(SSL_CERTIFICATE).toURI().toURL();
             if (url == null) {
                 throw new Exception();
             }
@@ -120,6 +192,13 @@ public class WebappApplication extends SpringBootServletInitializer {
             }
         }
 
+        /**
+         * This method creates the Key Store File
+         *
+         * @param rootX509Certificate - the SSL certificate to be stored in the KeyStore
+         * @return
+         * @throws Exception
+         */
         private static File createKeyStoreFile(X509Certificate rootX509Certificate) throws Exception {
             File keyStoreFile = File.createTempFile(KEY_STORE_FILE_PREFIX, KEY_STORE_FILE_SUFFIX);
             try (FileOutputStream fos = new FileOutputStream(keyStoreFile.getPath())) {
@@ -130,7 +209,8 @@ public class WebappApplication extends SpringBootServletInitializer {
             }
             return keyStoreFile;
         }
-
     }
+
+
     }
 
