3220650e2fd405403f7ab7c02a64bc81c0bb3e67
wanlaya.c <wanlaya.c@zealtechinter.com>
2022-11-15 10:15:42 +0700

    config prod

--- src/main/java/com/zti/kha/SwaggerConfig.java
+++ src/main/java/com/zti/kha/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.zti.kha;
 
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,16 +15,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig implements EnvironmentAware {
+    private Environment environment;
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                 .enable(Boolean.parseBoolean(environment.getProperty("use-swagger")));
+
     }
 
 
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
 
+    }
 }
