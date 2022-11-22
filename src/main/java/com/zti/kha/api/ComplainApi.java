4ac0523658854d122627d2351b0cf6afb0618df3
wanlaya.c <wanlaya.c@zealtechinter.com>
2022-11-22 17:01:55 +0700

    prod

--- src/main/java/com/zti/kha/api/ComplainApi.java
+++ src/main/java/com/zti/kha/api/ComplainApi.java
@@ -64,11 +64,11 @@ public class ComplainApi extends CommonApi {
     ) throws PostExceptions, ParseException {
 
         Properties props = new Properties();
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.enable", "false");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
