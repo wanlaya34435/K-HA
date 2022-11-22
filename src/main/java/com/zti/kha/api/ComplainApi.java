4ea3173a4bb443b59998ad7ba98ab6aad1065376
wanlaya.c <wanlaya.c@zealtechinter.com>
2022-11-22 17:06:04 +0700

    prod

--- src/main/java/com/zti/kha/api/ComplainApi.java
+++ src/main/java/com/zti/kha/api/ComplainApi.java
@@ -68,7 +68,7 @@ public class ComplainApi extends CommonApi {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.ssl.trust", "*");
+
 
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
